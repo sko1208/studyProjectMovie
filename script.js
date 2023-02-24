58509516130c8df203d9fb39c1bdd07982b07d76
sko1208 <skosam1208@gmail.com>
2023-02-24 17:57:37 +0200

    finishe first q

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let numberOfFilms;
-while (numberOfFilms == undefined || numberOfFilms == ' ' || numberOfFilms == 0 || isNaN(numberOfFilms)) {
+while (numberOfFilms == undefined || numberOfFilms == '' || numberOfFilms == 0 || isNaN(numberOfFilms)) {
   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 }
 
@@ -11,11 +11,21 @@ const personalMovieDB = {
   provet: false    
 };
 
-// for (let i = 0; i < 2; i++) {
-//   let lastMovie = prompt('Один из последних просмотренных фильмов?','')
-//   let gradeMovie = +prompt('На сколько оцените его?','');
-//   personalMovieDB.movies[lastMovie] = gradeMovie;
-// }
+for (let i = 1; i < 3; i++) {
+  let lastMovie = prompt('Один из последних просмотренных фильмов?','');
+  if (lastMovie == '' || lastMovie == null || lastMovie.length == 51) {
+    i--;
+  }
+  for (let k = 0; k < 1; k++) {
+    let gradeMovie = +prompt('На сколько оцените его?','');
+    personalMovieDB.movies[lastMovie] = gradeMovie;
+  }
+  
+}
+  
+  
+  
+
 
 
 console.log(personalMovieDB);
